a10c11d5bbf224479e6dbafef5236437733006be
Steve Okyere <Steveokyere910@gmail.com>
2026-09-08 18:35:20 +0000

    Explain all first-time user controls

--- sw.js
+++ sw.js
@@ -23,12 +23,12 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "valcare-v8";
+const CACHE_NAME = "valcare-v9";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=21",
-  "./app.jsx?v=24",
+  "./app.jsx?v=25",
   "./firebase.js",
   "./manifest.webmanifest",
   "./icon-192.png",
